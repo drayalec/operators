6317d54d7939e59fc8decbf3f9516bbc9609c73c
drayalec <31783033+drayalec@users.noreply.github.com>
2018-01-25 02:01:19 -0600

    Initial commit

---
+++ src/com/example/java/Main.java
@@ -0,0 +1,18 @@
+package com.example.java;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        boolean bTrue = true;
+        boolean bFalse = false;
+
+
+        System.out.println("5 + 7 = 12  " + bTrue);
+        System.out.println("15 - 6 = 8  " + bFalse);
+        System.out.println("7 * 3 = 21  " + bTrue);
+        System.out.println("15 / 3 =  5  " + bTrue);
+        System.out.println("12 % 5 = 2  " + bTrue);
+
+    }
+}
